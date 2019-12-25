e3e31c83ca96d015b48693e2166ef4e6382bc65c
Achileas Gebriel <achigalatis@hotmail.com>
2019-12-25 09:54:00 +0300

    Chapter 7: Textures, question no.1 Solution

--- Graphics/Shaders/fragment.glsl
+++ Graphics/Shaders/fragment.glsl
@@ -13,5 +13,5 @@ void main()
 {
 //    FragmentColor = vec4(0.2f, green, 0.5f, 1.0f);
 //    FragmentColor = vec4(ourColor, 1.0f);
-    FragmentColor = mix(texture(texture1, TextureCoord), texture(texture2, TextureCoord), 0.2);
+    FragmentColor = mix(texture(texture1, TextureCoord), texture(texture2, TextureCoord*vec2(-1.0,1.0)), 1.0);
 }
